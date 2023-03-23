7a7d3b36bd7b628b9cee783010b8c5640f4de29e
DevonGifford <DevonGifford@outlook.com>
2023-03-24 00:03:34 +0100

    3

---
+++ ex3.py
@@ -0,0 +1,31 @@
+print("I will now count my chickens")
+
+#This is simply printing a 'name' and then a mathamatical equation in the same line
+print("Hens", 25 + 30/6)
+print("Roosters", 100 - 25*3%4)
+
+#THis is simply printing the a sentance and then getting the iterpreter to do a simple calculation
+print("Now I will count the eggs:")
+print (3+2+1+5+4%2-1/4+6)
+
+#The same as above
+print("Is it true that 3 + 2 < 5 - 7?")
+print (3 + 2 < 5 - 7)
+
+#Printing a sentence asking a simple math question, followed by a simple calculation
+print ("What is 3 + 2?", 3 +2)
+print("What is 5 - 7?",5-7)
+print("Oh, that's why it's False.")
+
+#Printing a sentence asking a simple math question, followed by a simple greater than , less than or 'equal too or less than'statements.
+print("How about some more...")
+
+print("Is it gereater?", 5>-2)
+print("Is it gereater or equal?", 5 >= -2)
+
+print("Is it less or equal?",5<= -2)
+
+
+#Here is an example of something I need to calculate using floating point integers.
+print("I have a limited runway of funds, \tI have 9.2k in savings\tMy minimum monthly costs are approximatley 872.5 euros a month")
+print("Therefore I have approximatley",9200/872.5, "months remaining")
